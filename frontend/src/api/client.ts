290dcb53892c86599ee4980162f7da56b9911d1f
Aarushi Daksh <aarushi312004@gmail.com>
2025-11-15 16:54:36 +0530

    m

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -9,9 +9,6 @@ const API_URL =
 export const api = axios.create({
 <<<<<<< HEAD
   baseURL: API_URL,
-=======
-  baseURL: import.meta.env.VITE_API_URL || "https://todo-list-application-backend0.onrender.com/api",
->>>>>>> cf7277573539faf8aedd052e17a5b831d21f34b1
 });
 
 api.interceptors.request.use((config: InternalAxiosRequestConfig) => {
